1e16fd4144db1b5f15f5ec6c62e0d35791b9654d
Zakiah07 <zakiah_bloom@yahoo.com>
2022-06-17 22:57:34 +0800

    drawing app 55%

---
+++ 8. drawing-app/script.js
@@ -0,0 +1,57 @@
+const canvas = document.getElementById("canvas");
+const increaseBtn = document.getElementById("increase");
+const decreaseBtn = document.getElementById("decrease");
+const ctx = canvas.getContext("2d");
+
+let size = 20;
+let isPressed = false;
+
+canvas.addEventListener("mousedown", () => {
+    isPressed = true;
+});
+
+canvas.addEventListener("mouseup", () => {
+    isPressed = false;
+});
+
+canvas.addEventListener("mousemove", (e) => {
+    if (isPressed) {
+        const x = e.offsetX;
+        const y = e.offsetY;
+
+    drawCircle(x, y);
+    }
+});
+
+function drawCircle(x, y) {
+  ctx.beginPath();
+  ctx.arc(x, y, size, 0, Math.PI * 2);
+  ctx.fill();
+}
+
+increaseBtn.addEventListener('click', () => {
+    size += 5;
+
+    if(size > 50) {
+        size = 50;
+    }
+});
+
+decreaseBtn.addEventListener('click', () => {
+    size -= 5;
+
+    if(size < 5) {
+        size = 5;
+    }
+});
+
+
+// function draw() {
+//     ctx.clearRect(0, 0, canvas.clientWidth, canvas.height);
+
+//     drawCircle(x++, y);
+
+//     requestAnimationFrame(draw);
+// }
+
+// draw();
